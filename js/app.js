b53aa46cb3dafa8e4f099b48d4aa4fac24802027
Daniello2301 <daniel.lopera@est.iudigital.edu.co>
2021-12-06 15:41:00 -0500

    Form Validate Using javaScript

---
+++ js/app.js
@@ -0,0 +1,66 @@
+/* in inicializate form for create of eventlistener submit */
+document.getElementById("form").addEventListener("submit", validate);
+
+/*Boolean Atribute validate for alert message  */
+
+function validate(event){
+    
+    let enviar = true;
+
+    /* the inputs is called for Id for use the text in for keyboard */
+    const producto = document.getElementById('name_producto').value
+    const price = document.getElementById('price').value
+    const stock = document.getElementById('stock').value
+
+    /* Se llaman los span donde se adjuntaran los mensajes de validacion */
+    const productoMessage = document.getElementById('name_validate');
+    const priceMessage = document.getElementById('price_validate');
+    const stockMessage = document.getElementById('stock_validate');
+
+    /* Conditions for validate the information  */    
+
+    /* Validation for input name product */
+    if(producto == ""){
+
+        productoMessage.innerHTML = "Campo requerido";
+        enviar = false;
+    }
+
+    /* Validation for input price */
+    if(price == ""){
+
+        priceMessage.innerHTML = "Campo requerido";
+        enviar = false;
+
+    }else if(price <= 0){
+
+        priceMessage.innerHTML = "El precio debe ser mayor a 0";
+        enviar = false;
+
+    }
+
+    /* Validation for input stock */
+    if(stock == ""){
+
+        stockMessage.innerHTML = "Campo requerido";
+        enviar = false;
+
+    }else if(stock <= 0){
+
+        stockMessage.innerHTML = "El stock del porducto debe ser mayor a 0"
+        enviar = false;
+
+    }
+
+    /* Conditional show alert confirm */
+    if(enviar){
+
+        confirm("Producto agregado");
+
+    }
+
+    /* Validation enviar is false, then fields are no clean */
+    if(enviar == false){
+        event.preventDefault();
+    }
+}
